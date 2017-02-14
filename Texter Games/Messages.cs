4b72a7745089b841030cb8a1bf1cecb9643df3e2
MoSadie <sean@cushmantech.net>
2017-02-14 14:43:13 -0800

    Better ready a cannon. I misspelled "connon"

--- Texter Games/Messages.cs
+++ Texter Games/Messages.cs
@@ -12,7 +12,7 @@ namespace Texter_Games
         static string[] attackMessage = { "{0} lunged from behind a bush at {1}, attacking them with a(n) {2}.", "While {1} was least excepting it, {0} attacked with a(n) {2}" };
         static string[] attackWinMessage = { "As {0} watched, {1} drew their last breath after {0} attacked them with a(n) {2}.", "{1} fell to the ground, a(n) {2}, delivered by {0}, between their eyes." };
         static string[] attackLoseMessage = { "{1} defended perfecly advoiding {0}'s deadly attack with a(n) {2}", "{0} tripped while trying to attack {1} with a(n) {2}. {1} just stood and laughed, then ran away." };
-        static string[] deathMessage = { "{0} was done with the games and jumped of the nearest cliff and died.", "{0} ate some \"blueberries\" and died.", "Oops, guess that plant that {0} ate was actually poisonous. Better ready a connon." };
+        static string[] deathMessage = { "{0} was done with the games and jumped of the nearest cliff and died.", "{0} ate some \"blueberries\" and died.", "Oops, guess that plant that {0} ate was actually poisonous. Better ready a cannon." };
         static string[] gainItemMessage = { "{0} went searching and found a(n) {1} nearby!", "{0} searched a nearby cave and found a(n) {1}!" };
 
         public static void printNothingMessage(Contestant contestant)
